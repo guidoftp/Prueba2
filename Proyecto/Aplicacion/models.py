63f6b49c48efb2e06fce11b9a38782b9b024fdb3
Guido Andres Araya Gutierrez <guidoandres62@gmail.com>
2024-10-25 19:46:05 -0300

    Mejora del Index y agg una simple img

--- Proyecto/Aplicacion/models.py
+++ Proyecto/Aplicacion/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Juego(models.Model):
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=500)
     fecha_lanz = models.DateField()
-    desarrolladora = models.CharField(max_length=50)
+    desarrolladora = models.CharField(max_length=100)
     precio = models.FloatField(validators=[MinValueValidator(0)])
